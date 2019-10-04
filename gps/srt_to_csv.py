8e0dc67664d0cd8575620e9c22804a0166dba82e
Braian G. Soullier <55555803+Braso32@users.noreply.github.com>
2019-10-04 00:28:53 -0300

    Add files via upload

---
+++ gps/srt_to_csv.py
@@ -0,0 +1,46 @@
+#!/usr/bin/env python3
+# -*- coding: utf-8 -*-
+"""
+Created on Wed Oct  2 18:11:31 2019
+
+@author: ulises
+"""
+
+
+
+
+import pandas as pd
+import numpy as np
+import glob
+from time import time
+
+
+def srt2csv(path = '/home/ulises/00_Doctorado/MedicionesDrone/FAUBA_190904/'):
+	'''convierte los srt dentro del path en csv'''
+	a0 = time()
+	srtFiles=glob.glob(path+'**/*.SRT')
+	for thisSubtFile in srtFiles:
+	
+		fil= open(thisSubtFile)
+		subt=fil.read().split('\n\n')
+	
+		df_subt = pd.DataFrame(columns = ['frameNumber','dateTime','lat','lon'])
+		for i in range(len(subt)):
+			l 			= subt[i].split('[')
+			if l[0] != '':
+				l0sp 		= l[0].split('\n')
+				
+				datetime 	= ','.join(l0sp[-2].split(',')[:-1])
+				frNum 		= np.int32(l0sp[2].split(':')[1].split(',')[0])
+				la 			= l[-2].replace(']','').replace(' ','').split(':')[-1]
+				lo 			= l[-1].replace(']','').replace(' ','').split(':')[-1].replace('</font>','')
+				df_subt = df_subt.append({'frameNumber':frNum,
+								 'dateTime':pd.to_datetime(datetime),
+								 'lat':np.float(la),
+								 'lon':np.float(lo)},ignore_index=True)
+		print('tiempo parcial')
+		df_subt.to_csv(thisSubtFile.split('.SRT')[0]+'.csv')
+	
+	a1 = time()
+	
+	print('esto tardo: ', a1-a0,' segundos en correr sobre ',len(srtFiles),' subtitulos')
